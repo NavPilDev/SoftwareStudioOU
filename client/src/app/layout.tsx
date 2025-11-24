ff3fb61f1e90ff8e6edcd0562ec5bbd41b6dc97b
NavPilDev <abhinavpillai04@gmail.com>
2025-11-23 23:56:50 -0600

    Add analytics

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import { Layout, FixedPlugin } from "@/components";
+import { Analytics } from "@vercel/analytics/next";
 
 const roboto = Roboto({
   subsets: ["latin"],
